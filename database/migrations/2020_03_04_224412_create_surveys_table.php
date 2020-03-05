f6ec030b50f7da82828ba88938bcd0cda424f202
Jacob Henning <jacobshenning@gmail.com>
2020-03-04 16:08:32 -0800

    Initial commit

---
+++ database/migrations/2020_03_04_224412_create_surveys_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSurveysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surveys', function (Blueprint $table) {
+            $table->id();
+            $table->string('identifier');
+            $table->integer('question_count');
+            $table->integer('correct_count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('surveys');
+    }
+}
